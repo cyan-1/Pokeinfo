7488809c0cef42d89e84a25af1b8d5bdd5f925a1
cyan-1 <00ffffcyanide@gmail.com>
2023-08-07 13:43:58 -0400

    added loading state to pokedex, made the pokedex design more responsive for smaller devices

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,38 @@
-function renderPokedex() {
-    const cards = document.getElementById("content-cards");
-    cards.innerHTML = "";
-
-    for (let i = 1; i <= 151; i++) {
-        const card = document.createElement("div");
-        card.classList.add("card");
-
-        const pokemonImg = document.createElement("img");
-        const pokemonName = document.createElement("h4");
-
-        fetch(`https://pokeapi.co/api/v2/pokemon/${i}/`)
-            .then(response => response.json())
-            .then(data => {
-                pokemonImg.src = data.sprites.front_default;
-                pokemonName.textContent = data.name;
-            }).catch(error => {
-                console.error(error);
-            });
-
-        card.appendChild(pokemonImg);
-        card.appendChild(pokemonName);
-        cards.appendChild(card);
-    }
+async function fetchPokemonData(i) {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}/`);
+    return response.json();
 }
 
-window.addEventListener("load", function() {
-    renderPokedex();
-});
+async function renderPokedex() {
+    const cards = document.getElementById("content-cards");
+    cards.innerHTML = "Loading...";
+
+    try {
+        const data = [];
+        for (let i = 1; i <= 151; i++) {
+            data.push(fetchPokemonData(i));
+        }
+
+        const pokemonData = await Promise.all(data);
+
+        cards.innerHTML = "";
+
+        for (let i = 0; i < pokemonData.length; i++) {
+            const card = document.createElement("div");
+            card.classList.add("card");
+
+            const pokemonImg = document.createElement("img");
+            pokemonImg.src = pokemonData[i].sprites.front_default;
+
+            const pokemonName = document.createElement("h4");
+            pokemonName.textContent = pokemonData[i].name;
+
+            card.appendChild(pokemonImg);
+            card.appendChild(pokemonName);
+            cards.appendChild(card);
+        }
+    } catch (err) {
+        err;
+    }
+}
+renderPokedex();
